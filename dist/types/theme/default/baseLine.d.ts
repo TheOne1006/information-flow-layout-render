662fa657d7645137479f04c20dc4b763ab428c72
theone1006 <297190869@qq.com>
2017-08-24 14:47:31 +0800

    fix(debug):remove dist

--- dist/types/theme/default/baseLine.d.ts
+++
@@ -1,3 +0,0 @@
-import "core-js/fn/object/assign";
-declare function configCreate(winWidth: number, customStyles: any): any;
-export default configCreate;
--- dist/types/theme/default/srcTime.d.ts
+++
@@ -1,6 +0,0 @@
-import "core-js/fn/object/assign";
-declare const _default: {
-    configWrapCreate: (winWidth: number, customStyles: any) => any;
-    configItemCreate: (winWidth: number, customStyles: any) => any;
-};
-export default _default;
