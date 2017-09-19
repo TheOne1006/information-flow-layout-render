ba880c139d6f2b0b3cee203128c29b6f8a7e966b
theone1006 <297190869@qq.com>
2017-09-19 11:22:33 +0800

    demo(demo): update demo

--- dist/types/sections/imgsSection.d.ts
+++ dist/types/sections/imgsSection.d.ts
@@ -3,7 +3,7 @@ import BaseSection from "./baseSection";
 declare class ImgsSection extends BaseSection {
     createWrapper(winWidth: number, href?: string, target?: string, title?: string, touchCallback?: Function): any;
     createTitle(winWidth: number, title: string): any;
-    createContent(winWidth: number, images: string[]): any[];
+    createContent(winWidth: number, images: string[]): any;
     createRemark(winWidth: number, type: number, desc?: string, src?: string, time?: string): any;
     render(fragment: DocumentFragment, winWidth: number, adItem: IadItemModel, redirectUrl?: string, touchCallback?: Function): void;
 }
